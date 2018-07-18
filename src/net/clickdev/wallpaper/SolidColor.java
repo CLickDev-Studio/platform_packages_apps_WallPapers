bb97d77664e996d193315a4636a39e5882a425c8
Zecozhang <ryzbt41@gmail.com>
2018-07-18 15:59:40 +0800

    First add by update

--- src/net/clickdev/wallpaper/SolidColor.java
+++ src/net/clickdev/wallpaper/SolidColor.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package org.aospextended.aexpapers;
+package net.clickdev.wallpaper;
 
 import android.app.Activity;
 import android.app.AlertDialog;
